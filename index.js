e1b073bf14053a2d449695fa877a6c8d4493f3d4
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-19 03:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code in this file!
+function scuberGreetingForFeet (199) {
+  if (scuberGreetingForFeet <= 400) {
+    return "This one is on me!"
+  }
+  else if (scuberGreetingForFeet > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (scuberGreetingForFeet > 2500) {
+    return "No can do."
+  }
+}
+
+const place = "NYC";
+function ternaryCheckCity ("Pitsburgh") {
+  if (place === true) {
+    return "Ok, sounds good."
+  }
+  else (place === false){
+    return "No go."
+  }
+}
